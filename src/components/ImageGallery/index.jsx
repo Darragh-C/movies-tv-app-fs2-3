9596050087f8cc02c030930e01da2abd3119c115
Darragh Conneely <darraghc23@gmail.com>
2023-08-08 20:14:58 +0100

    functioning image gallery with img scaling issues

--- src/components/ImageGallery/index.jsx
+++ src/components/ImageGallery/index.jsx
@@ -8,15 +8,15 @@ const styles = {
     alignItems: "center",
     minWidth: "80%",
     overflowX: 'auto',
-    paddingTop: 30,
-    paddingLeft: 40,
+    paddingTop: 20,
+
   },
   playlistContainer: {
     border: "1px solid #ccc",
     borderRadius: "18px",
     boxShadow: "0 5px 10px rgba(0, 0, 0, 0.1)", 
-    padding: "30px",
-    minWidth: "80%",
+ 
+    minWidth: "100%",
   },
   cardContainer: {
     display: 'flex',
@@ -32,39 +32,42 @@ const styles = {
     justifyContent:"left",
   },
   titleDiv: {
-    paddingBottom: "20px"
+    paddingTop: "20px"
   },
   image: {
-    width: "100%",
+    width: "40%",
   }
 };
 
 const ImageGallery = ({ galleryImages }) => {
   return (
-    <div style={styles.frameContainer}>
-      <div style={styles.playlistContainer}>
-        <div style={styles.titleDiv}>
-          <Typography sx={styles.typeography} variant="h3" component="p">
-            User image gallery
-          </Typography>
-        </div>
-        {galleryImages && 
-          <div style={{ overflowX: 'auto' }}>
-            <div style={styles.cardContainer}>
-              {galleryImages.map((imagePath) => (
-                <div>
-                  <img
-                    style={styles.image}
-                    src={`https://res.cloudinary.com/${import.meta.env.VITE_CLOUD_NAME}/image/upload/${imagePath}`}
-                    alt={imagePath}
-                  />
-                </div>
-              ))}
-            </div>
-          </div>
-        }
+    <>
+      <div style={styles.titleDiv}>
+        <Typography sx={styles.typeography} variant="h5" component="p">
+          User image gallery
+        </Typography>
       </div>
-    </div>
+      <div style={styles.frameContainer}>
+        <div style={styles.playlistContainer}>
+          {galleryImages && 
+            <div style={{ overflowX: 'auto' }}>
+              <div style={styles.cardContainer}>
+                {galleryImages.map((imagePath) => (
+                  <div>
+                    <img
+                      style={styles.image}
+                      src={`https://res.cloudinary.com/${import.meta.env.VITE_CLOUD_NAME}/image/upload/${imagePath}`}
+                      alt={imagePath}
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+          }
+        </div>
+      </div>    
+    </>
+
   );
 };
 
